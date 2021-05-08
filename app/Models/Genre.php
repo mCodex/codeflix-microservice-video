54d347d384a531fc74e4933e6378662ff1459701
Mateus Andrade <mat.andrade@live.com>
2021-05-08 11:45:51 -0300

    Added genre controller

--- app/Models/Genre.php
+++ app/Models/Genre.php
@@ -6,15 +6,11 @@ use App\Models\Traits\Uuid;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Category extends Model
+class Genre extends Model
 {
     use SoftDeletes, Uuid;
     protected $fillable = ['name', 'description', 'is_active'];
     protected $dates = ['deleted_at'];
-    protected $casts = [
-        'id' => 'string'
-    ];
-
     protected $keyType = 'string';
 
     public $incrementing = false;
