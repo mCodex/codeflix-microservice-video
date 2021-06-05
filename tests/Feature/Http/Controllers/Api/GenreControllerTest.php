e9cf985709ceb4b7ef9010dbba76ecc1a9f07f2a
Mateus Andrade <mat.andrade@live.com>
2021-06-05 09:41:48 -0300

    Added genre's http test

---
+++ tests/Feature/Http/Controllers/Api/GenreControllerTest.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Api;
+
+use App\Models\Genre;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\TestResponse;
+
+use Tests\TestCase;
+
+use Tests\Traits\TestValidations;
+use Tests\Traits\TestSaves;
+
+class GenreControllerTest extends TestCase
+{
+    use DatabaseMigrations, TestValidations, TestSaves;
+
+    private $genre;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->genre = factory(Genre::class)->create();
+    }
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testIndex()
+    {
+        $response = $this->get(route('genres.index'));
+
+        $response->assertStatus(200)->assertJson([$this->genre->toArray()]);
+    }
+
+    public function testShow()
+    {
+        $response = $this->get(route('genres.show', ['genre' => $this->genre->id]));
+
+        $response->assertStatus(200)->assertJson($this->genre->toArray());
+    }
+
+    public function testInvalidationData()
+    {
+        $data = [
+            'name' => ''
+        ];
+
+        $this->assertInvalidationInStoreAction($data, 'required');
+        $this->assertInvalidationInUpdateAction($data, 'required');
+
+        //----------
+
+        $data = [
+            'name' => str_repeat('a', 500),
+        ];
+
+        $this->assertInvalidationInStoreAction($data, 'max.string', ['max' => 255]);
+        $this->assertInvalidationInUpdateAction($data, 'max.string', ['max' => 255]);
+
+
+        $data = [
+            'is_active' => 'a'
+        ];
+
+        $this->assertInvalidationInStoreAction($data, 'boolean');
+        $this->assertInvalidationInUpdateAction($data, 'boolean');
+    }
+
+    protected function assertInvalidationRequired(TestResponse $response)
+    {
+        $this->assertInvalidationFields($response, ['name'], 'required');
+        $response
+            ->assertJsonMissingValidationErrors(['is_active']);
+    }
+
+    protected function assertInvalidationMax(TestResponse $response)
+    {
+        $this->assertInvalidationFields($response, ['name'], 'max.string', ['max' => 255]);
+    }
+
+    protected function assertInvalidationBoolean(TestResponse $response)
+    {
+        $this->assertInvalidationFields($response, ['is_active'], 'boolean', ['is active']);
+    }
+
+    public function testStore()
+    {
+
+        $data = [
+            'name' => 'test',
+        ];
+
+        $response = $this->assertStore($data, $data + ['is_active' => true, 'deleted_at' => null]);
+        $response->assertJsonStructure([
+            'created_at', 'deleted_at'
+        ]);
+
+
+        $data = [
+            'name' => 'test',
+            'is_active' => false
+        ];
+
+        $this->assertStore($data, $data + ['is_active' => false, 'deleted_at' => null]);
+    }
+
+
+    public function testUpdate()
+    {
+        $data = [
+            'name' => 'test',
+            'is_active' => true
+        ];
+
+        $response = $this->assertUpdate($data, $data + ['deleted_at' => null]);
+
+        $response->assertJsonStructure([
+            'created_at', 'deleted_at'
+        ]);
+    }
+
+    public function testDestroy()
+    {
+        $response = $this->json(
+            'DELETE',
+            route('genres.destroy', [
+                'genre' => $this->genre->id
+            ])
+        );
+
+        $response->assertStatus(204);
+
+        $this->assertNull(Genre::find($this->genre->id));
+
+        $this->assertNotNull(Genre::withTrashed()->find($this->genre->id));
+    }
+
+    protected function routeStore()
+    {
+        return route('genres.store');
+    }
+
+
+    protected function routeUpdate()
+    {
+        return route('genres.update', ['genre' => $this->genre->id]);
+    }
+
+    protected function model()
+    {
+        return Genre::class;
+    }
+}
