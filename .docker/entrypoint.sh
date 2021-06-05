77560be8d00fe2a2f513903f0fd57cd72bd590da
Mateus Andrade <mat.andrade@live.com>
2021-06-05 13:00:27 -0300

    Refatoring repo's structure preparing it for React

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -2,6 +2,17 @@
 
 #On error no such file entrypoint.sh, execute in terminal - dos2unix .docker\entrypoint.sh
 chown -R www-data:www-data .
+
+cd backend
+
+if [ ! -f ".env"]; then
+    cp .env.example .env
+fi
+
+if [ ! -f ".env.testing"]; then
+    cp .env.testing.example .env.testing
+fi
+
 composer install
 php artisan key:generate
 php artisan migrate
