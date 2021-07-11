1597336f637a135c22a4f5bb924e09518db6d7d3
Mateus Andrade <mat.andrade@live.com>
2021-07-11 11:01:00 -0300

    Added with trashed in Video and Genre model, also improving Genres and Videos table seeder

--- backend/app/Models/Genre.php
+++ backend/app/Models/Genre.php
@@ -21,6 +21,6 @@ class Genre extends Model
 
     public function categories()
     {
-        return $this->belongsToMany(Category::class);
+        return $this->belongsToMany(Category::class)->withTrashed();
     }
 }
--- backend/app/Models/Video.php
+++ backend/app/Models/Video.php
@@ -36,11 +36,11 @@ class Video extends Model
 
     public function categories()
     {
-        return $this->belongsToMany(Category::class);
+        return $this->belongsToMany(Category::class)->withTrashed();
     }
 
     public function genres()
     {
-        return $this->belongsToMany(Genre::class);
+        return $this->belongsToMany(Genre::class)->withTrashed();
     }
 }
