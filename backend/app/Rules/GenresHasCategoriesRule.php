1fef367efc008a21c36330db37c0aa81e4794730
Mateus Andrade <mat.andrade@live.com>
2021-07-12 19:07:29 -0300

    Added GenreHasCategoriesRuleTest

--- backend/app/Rules/GenresHasCategoriesRule.php
+++ backend/app/Rules/GenresHasCategoriesRule.php
@@ -3,7 +3,7 @@
 namespace App\Rules;
 
 use Illuminate\Contracts\Validation\Rule;
-use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 
 class GenresHasCategoriesRule implements Rule
